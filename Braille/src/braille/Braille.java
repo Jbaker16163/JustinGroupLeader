4a8e6ffdd1ea7799b8fa710da59faaefb19fd4a2
Jbaker16163 <56654758+Jbaker16163@users.noreply.github.com>
2020-02-07 15:46:00 -0500

    Add files via upload

---
+++ Braille/src/braille/Braille.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package braille;
+
+/**
+ *
+ * @author Justin Baker
+ */
+public class Braille {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String J = "\u281a";
+        String u = "\u2825";
+        String s = "\u2817";
+        String t = "\u281e";
+        String i = "\u280a";
+        String n = "\u2825";
+        
+        System.out.println(J + u + s + t + i + n);
+    }
+    
+}
